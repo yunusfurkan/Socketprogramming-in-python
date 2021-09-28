a7f3fde990a3c5daa0d61f40effd40f718aa0b2d
Yunus Furkan Öztürk <90008976+yunusfurkan@users.noreply.github.com>
2021-09-28 22:02:58 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,31 @@
+import socket, cv2, pickle, struct
+
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+host_name= socket.gethostname()
+host_ip = socket.gethostbyname(host_name)
+print('HOST IP:',host_ip)
+port = 9999
+socket_adress = (host_ip,port)
+
+#socket bind
+server_socket.bind(socket_adress)
+
+#socket listen
+server_socket.listen(5)
+print("Listening at:",socket_adress)
+#socket accept
+
+while True:
+    client_socket, addr=server_socket.accept()
+    print('GET CONNECTION FROM:',addr)
+    if client_socket:
+        vid=cv2.VideoCapture('C:/PİYU/video/VID-20200827-WA0013.mp4')
+        while(vid.isOpened()):
+            img,frame= vid.read()
+            a=pickle.dumps(frame)
+            message = struct.pack("Q",len(a))+a
+            client_socket.sendall(message)
+            cv2.imshow('TRANSMIT VIDEO',frame)
+            key=cv2.waitKey(1) & 0xFF
+            if key ==ord('q'):
+                client_socket.close()
